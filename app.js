f1f46aafcb0ad484891316ddc52b6770c7125d54
HammadAhmad221 <128469279+HammadAhmad221@users.noreply.github.com>
2024-10-23 20:54:20 +0500

    remove db and add all data again from site ground than modify it again to check inbox after every 3 minuts for unread messages create leads from them and imark them read

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ require("dotenv/config"); // Environment variables
 
 // Route imports
 const authRoutes = require("./routes/auth");
-const vehicleRoutes = require('./routes/VehicleRoutes');
-const invoiceRoutes = require('./routes/InvoiceRoutes');
-const loadRoutes = require('./routes/LoadRoutes');
-const driverRoutes = require('./routes/DriverRoutes');
-const userRoutes = require('./routes/UserRoutes');
-const customerRoutes = require('./routes/CustomerRoutes');
-const counts = require('./routes/counts');
-const inputMails = require('./routes/InboxMails');
-const countMails = require('./routes/countEmails');
-const fetchEmailsAndSaveLeads = require('./autoGenerateCustomers/sitegroundCustomers');
+const vehicleRoutes = require("./routes/VehicleRoutes");
+const invoiceRoutes = require("./routes/InvoiceRoutes");
+const loadRoutes = require("./routes/LoadRoutes");
+const driverRoutes = require("./routes/DriverRoutes");
+const userRoutes = require("./routes/UserRoutes");
+const customerRoutes = require("./routes/CustomerRoutes");
+const counts = require("./routes/counts");
+const inputMails = require("./routes/InboxMails");
+const countMails = require("./routes/countEmails");
+const fetchEmailsAndSaveLeads = require("./autoGenerateCustomers/sitegroundCustomers");
 
 // -> Middlewares
 app.use(cors());
@@ -26,27 +26,25 @@ app.use(express.json());
 
 // -> Route Middlewares
 app.use("/api/user", authRoutes);
-app.use('/api/vehicles', vehicleRoutes);
-app.use('/api/invoices', invoiceRoutes);
-app.use('/api/loads', loadRoutes);
-app.use('/api/drivers', driverRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/customers',customerRoutes);
-app.use('/api/counts', counts );
-app.use('/api',inputMails);
-app.use('/emails',countMails);
+app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/loads", loadRoutes);
+app.use("/api/drivers", driverRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/counts", counts);
+app.use("/api", inputMails);
+app.use("/emails", countMails);
 
-
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 // Connect to Database
 mongoose.connect(process.env.DB_URL, () => {
   console.log("Connected to Database");
 });
 
 //autogenerate Customers
-setInterval(fetchEmailsAndSaveLeads,300000);
+setInterval(fetchEmailsAndSaveLeads, 300000);
 // fetchEmailsAndSaveLeads();
-
 
 // Starting the server
 app.listen(process.env.PORT, () => {
